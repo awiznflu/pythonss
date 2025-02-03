0bdded424e8ce9c088645bfb2fafd1d08ff8d0de
anna <a.weissenfluhs@icloud.com>
2025-02-03 11:49:26 -0500

    yuh

--- new.py
+++ new.py
@@ -1,8 +1,29 @@
-Anna = 15
+# Anna = 15
+#
+# num= int(input("Enter a number to divide by " + str(Anna) + ":"))
+#
+# Division = Anna/num
+#
+# print( str(Anna) + " divided by " + str(num) + " is " + str(Division))
 
-num= int(input("Enter a number to divide by " + str(Anna) + ":"))
 
-Division = Anna/num
+Chocolate = [ 'dark' , 'milk' , 'white' , ' toffee', 'hazelnut']
 
-print( str(Anna) + " divided by " + str(num) + " is " + str(Division))
+print(Chocolate)
 
+print(Chocolate[-1].title())
+
+countries = ['USA' , "Columbia" , "South Korea"]
+
+print (countries)
+
+print ("I am from " + str(countries[0]))
+
+
+countries[2] = "North Korea"
+
+print (countries[2])
+
+countries.append('China')
+
+print ("\nNew item added to list: " + str(countries[3]))
