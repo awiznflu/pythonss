1ab273e63a0016fa8c81dd960d55205f9fdf7b80
anna <a.weissenfluhs@icloud.com>
2025-02-10 11:51:17 -0500

    in class

---
+++ week 3.py
@@ -0,0 +1,29 @@
+# cars = 'BMW'
+#
+# 'bmw' != 'Bmw'
+# 'bmw' != 'BMW'
+#
+# if cars.lower() == 'bmw':
+#     print (cars.upper())
+#     print ('Wow they are different')
+#
+# if cars == 'altima':
+#     print("They are the same ")
+# else:
+#     print ('They are not the same')
+
+# age = 17
+#
+# if age <= 18:
+#     print ('You are underage')
+# else:
+#     print ('You are able to work')
+
+age1= 18
+age2 = 21
+
+if age1 >= 17 and age2 >= 21:
+    print ('True')
+else:
+    print('False')
+
