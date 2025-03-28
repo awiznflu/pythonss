9eab6c3fda26dfc1fd6d38c5c09bbca614be4bbd
anna <a.weissenfluhs@icloud.com>
2025-03-28 10:56:40 -0400

    in class

--- class card.py
+++
@@ -1,9 +0,0 @@
-class Car:
-    def __init__(self,make, model, year, ):
-        self.make = make
-        self.model = model
-        self.year = year
-    def display_info(self):
-        print(f"Car Deatils: {self.year} {self.model} {self.make}")
-my_car = Car ("Ford" , "Mustang" , "2020")
-my_car.display_info()
